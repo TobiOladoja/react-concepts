3a597e50001cce946b315fe58754818970d01552
TobiOladoja <tbfullstack@gmail.com>
2020-01-13 11:56:53 +0000

    DOM Events, Handling events on React elements

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ class App extends Component {
     name: 'Tobi',
     age: 21
   };
+  handleClick = e => {
+    // console.log(e.target);
+    console.log(this.state);
+  };
+
+  handleHover = e => {
+    console.log(e.target);
+  };
+  handleCopy = e => {
+    console.log('Try being original bro');
+  };
   render() {
     const { name, age } = this.state;
+    const { handleClick, handleHover, handleCopy } = this;
     return (
-      <div class='App'>
+      <div className='App'>
         <h1>
           Hi, my name is {name} and I am {age} years old.
         </h1>
+        <button onClick={handleClick}>Click Me</button>
+        <button onMouseOver={handleHover}>Hover me</button>
+        <p onCopy={handleCopy}>What we think, we become</p>
       </div>
     );
   }
