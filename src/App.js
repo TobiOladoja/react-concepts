7ef1109144e9e7228ea76b9ff1aac592211ee1e7
TobiOladoja <tbfullstack@gmail.com>
2020-01-13 10:30:30 +0000

    State for name and age, then destructuring using ES6 so that I only have to refer to it as name and age s as oppose to this.state.name and this.state.age

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: 'Tobi',
+    age: 21
+  };
   render() {
+    const { name, age } = this.state;
     return (
       <div class='App'>
-        <h1>yyy</h1>
+        <h1>
+          Hi, my name is {name} and I am {age} years old.
+        </h1>
       </div>
     );
   }
