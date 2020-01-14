5f266761d03df715a89e48db961714515b21fff8
TobiOladoja <tbfullstack@gmail.com>
2020-01-14 13:42:26 +0000

    Conditional output - only show if aged over 21

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -3,15 +3,19 @@ import React from 'react';
 const Ninjas = props => {
   const { ninjas } = props;
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className='ninja' key={ninja.id}>
-        <ul>
-          <li>Name: {ninja.name}</li>
-          <li>Age: {ninja.age}</li>
-          <li>Belt: {ninja.belt}</li>
-        </ul>
-      </div>
-    );
+    if (ninja.age > 21) {
+      return (
+        <div className='ninja' key={ninja.id}>
+          <ul>
+            <li>Name: {ninja.name}</li>
+            <li>Age: {ninja.age}</li>
+            <li>Belt: {ninja.belt}</li>
+          </ul>
+        </div>
+      );
+    } else {
+      return null;
+    }
   });
   return <div className='ninja-list'>{ninjaList}</div>;
 };
