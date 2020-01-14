05926b7093764a2313cced55ad4f35db5fede64a
TobiOladoja <tbfullstack@gmail.com>
2020-01-14 13:23:28 +0000

    UI Components without state made by functions

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className='ninja' key={ninja.id}>
-          <ul>
-            <li>Name: {ninja.name}</li>
-            <li>Age: {ninja.age}</li>
-            <li>Belt: {ninja.belt}</li>
-          </ul>
-        </div>
-      );
-    });
-    return <div className='ninja-list'>{ninjaList}</div>;
-  }
-}
+const Ninjas = props => {
+  const { ninjas } = props;
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className='ninja' key={ninja.id}>
+        <ul>
+          <li>Name: {ninja.name}</li>
+          <li>Age: {ninja.age}</li>
+          <li>Belt: {ninja.belt}</li>
+        </ul>
+      </div>
+    );
+  });
+  return <div className='ninja-list'>{ninjaList}</div>;
+};
 
 export default Ninjas;
