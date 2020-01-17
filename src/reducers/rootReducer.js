ed0f85ef48d0d1698b8bf597d47bf8667ab424f4
TobiOladoja <tbfullstack@gmail.com>
2020-01-17 18:37:02 +0000

    Redux: Access Redux Store Data From Components

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-  posts: []
+  posts: [
+    {
+      id: '1',
+      title: 'Good Life Title Heee',
+      body: "I'm bored so this is the big body!"
+    },
+    {
+      id: '2',
+      title: 'Decent Life Title Yoo',
+      body: "I'm okay I guess, so this is the big body!"
+    },
+    {
+      id: '3',
+      title: 'Excellent Life Title Hmm',
+      body: "I'm excited so this is the big body!"
+    }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
